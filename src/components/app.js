34ada220061187fdeb399a6f77e5b4296299a011
Denis Cantor <deniscantor7@gmail.com>
2022-10-10 18:14:30 +0200

    implemented all functionalities

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@ import Gif from './gif';
 import GifList from "./giflist";
 import giphy from "giphy-api";
 
+const GIPHY_API_KEY = 'KrtqBV6Bn4CZMjEDiCcEwKSlqzitJhOt'
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +13,9 @@ class App extends Component {
     this.state = {
       gifs: [],
       selectedGifId: "bW7WgIA6WBxXVYAXub"
-    }
+    };
+    this.search = this.search.bind(this);
+    this.selectGif = this.selectGif.bind(this);
   }
 
   search = (query) => {
@@ -26,8 +30,24 @@ class App extends Component {
     });
   }
 
-  render() {
 
+  // search(query) {
+  //   const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=10`
+  //   fetch(giphEndpoint).then(response => response.json()).then((data) => {
+  //     const gifs = data.data.map(giph => giph.id)
+  //     this.setState({
+  //       gifs: gifs
+  //     })
+  //   })
+  // }
+
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
     return (
       <div>
         <div className="left-scene">
@@ -37,7 +57,7 @@ class App extends Component {
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
 
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    // This is how you can access the input -- Controlled components
+    this.state = {
+      term: ""
+    }
+  }
+
   handleUpdate = (event) => {
+    this.setState({
+      term: event.target.value
+    });
     this.props.searchFunction(event.target.value);
   }
 
   render() {
     return (
-      <input type="text" className="form-control form-search"
-        onChange={this.handleUpdate}/>
+      <input
+        value={this.state.term}
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
