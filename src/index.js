a13026d990bf11e6df3dac628fd84958e0fb8c66
Denis Cantor <deniscantor7@gmail.com>
2022-10-14 09:44:31 +0200

    front end adjustments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { findAllByTestId } from '@testing-library/react';
-import React, { Component } from 'react';
-import ReactDOM, { hydrateRoot } from 'react-dom/client';
+// import { findAllByTestId } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from "./components/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
